d76581265b384a3ec347505f31c748b9d7da97c5
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-04-13 03:09:56 +0000

    [Coloqué estructira de cada fetch call]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,21 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
+
+	let getToDoURL = "https://playground.4geeks.com/todo/docs"
+
+	fetch(getToDoURL).then().then().catch()
+
+	fetch().then().then().catch()
+
+	fetch().then().then().catch()
+
+	fetch().then().then().catch()
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>To Do List!</h1>
 		</div>
 	);
 };
